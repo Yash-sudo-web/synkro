fc9b72fb809e07c4c5610454a6787c97dc9b6a4c
Yash-sudo-web <ymathur123@gmail.com>
2023-08-11 19:03:19 +0530

    updates

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,106 @@
-import React from 'react'
-import logo from '../assets/logo.png'
+import React from "react";
+import logo from "../assets/logo.png";
+import google from "../assets/google.jpg";
+import facebook from "../assets/facebook.png";
+import { useState } from "react";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [isEmailFilled, setIsEmailFilled] = useState(false);
+  const [pass, setPass] = useState("");
+  const [isPassFilled, setIsPassFilled] = useState(false);
+
+  const handlePassChange = (event) => {
+    const newPass = event.target.value;
+    setPass(newPass);
+    setIsPassFilled(newPass !== "");
+  };
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    setIsEmailFilled(newEmail !== "");
+  };
   return (
-    <div className="border-b-[1px] border-[#525252] text-white h-[60px] flex items-center">
-        <div className="w-[32px] flex flex-row ml-4"><img src={logo} alt="logo" /><div className='ml-2 text-3xl'>Synkro</div></div>
-    </div>
+    <>
+      <div className="border-b-[1px] border-[#525252] text-white h-[60px] flex items-center">
+        <div className="w-[32px] flex flex-row ml-4">
+          <img src={logo} alt="logo" />
+          <div className="ml-2 text-3xl">Synkro</div>
+        </div>
+      </div>
+      <div className="h-[80vh] flex items-center justify-center">
+        <div className="w-[1000px] h-[600px] bg-[#0a0a0a] flex flex-col items-center">
+          <div className="text-white text-[44px]">Log in to Synkro</div>
+          <br />
+          <div className="text-white">
+            <form className="mt-3">
+              <label>
+                Email<span className="text-[#a2fe65]">*</span>
+              </label>
+              <br />
+              <input
+                name="email"
+                type="email"
+                autoComplete="email"
+                className="bg-[#0a0a0a] pl-3 mt-1 mb-5 border border-[#525252] w-[340px] h-[32px] text-white custom-placeholder"
+                placeholder="test@mail.com"
+                value={email}
+                onChange={handleEmailChange}
+              ></input>
+              <br />
+              <label>
+                Password<span className="text-[#a2fe65]">*</span>
+              </label>
+              <br />
+              <input
+                name="password"
+                type="password"
+                className="bg-[#0a0a0a] pl-3 mt-1 mb-5 border border-[#525252] w-[340px] h-[32px] text-white custom-placeholder"
+                placeholder="Your Password"
+                value={pass}
+                onChange={handlePassChange}
+              ></input>
+              <br />
+              <div className="">Forgot your password?</div>
+              <div className="pt-5 pb-3">
+                <button
+                  className={`font-bold border border-[#525252] w-[340px] h-[50px] rounded-lg ${
+                    isEmailFilled && isPassFilled
+                      ? "bg-[#a2fe65] text-[#0a0a0a] hover:cursor-pointer"
+                      : "bg-[#141414] text-[#525252] cursor-not-allowed"
+                  }`}
+                  disabled={!isEmailFilled || !isPassFilled}
+                >
+                  Get Started
+                </button>
+              </div>
+            </form>
+            <div className="flex flex-row">
+              <hr class="h-px w-[144px] my-3 mr-4 bg-[#525252] border-0"></hr>Or
+              <hr class="h-px w-[144.5px] my-3 ml-4 bg-[#525252] border-0"></hr>
+            </div>
+            <div className="hover:cursor-pointer border border-[#525252] w-[340px] rounded-lg h-[50px] flex flex-row justify-between items-center mb-6">
+              <div className="ml-3">
+                <img width={24} height={24} src={google}></img>
+              </div>
+              <div className="mr-[27px]">
+                <p>Sign in with Google</p>
+              </div>
+              <div></div>
+            </div>
+            <div className="hover:cursor-pointer border border-[#525252] w-[340px] rounded-lg h-[50px] flex flex-row justify-between items-center mb-6">
+              <div className="ml-3">
+                <img width={24} height={24} src={facebook}></img>
+              </div>
+              <div className="mr-3">
+                <p>Sign in with Facebook</p>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    
-  )
-}
-
-export default Login
+export default Login;

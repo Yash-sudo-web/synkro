3b7da1d76f85d0e421d5bba7cb35186379f1331f
Yash-sudo-web <ymathur123@gmail.com>
2023-09-16 21:58:47 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
   };
   const handleLogin = async (event) => {
     event.preventDefault();
-    const response = await fetch("http://localhost:5000/api/user/login", {
+    const response = await fetch("https://synkro-backend.azurewebsites.net/api/user/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -38,11 +38,11 @@ const Login = () => {
   };
   const handleGoogleAuth  = async (e) => {
     e.preventDefault();
-    window.location.href = "http://localhost:5000/api/user/google"
+    window.location.href = "https://synkro-backend.azurewebsites.net/api/user/google"
   };
   const handleFacebookAuth  = async (e) => {
     e.preventDefault();
-    window.location.href = "http://localhost:5000/api/user/facebook"
+    window.location.href = "https://synkro-backend.azurewebsites.net/api/user/facebook"
   };
   return (
     <div className="w-screen h-screen">

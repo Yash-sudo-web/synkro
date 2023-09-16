bb42a26b4fab6175a8e769f83f922c9d8b820289
Yash-sudo-web <ymathur123@gmail.com>
2023-09-17 00:40:09 +0530

    Update Conversation.js

--- client/src/components/Conversation.js
+++ client/src/components/Conversation.js
@@ -8,7 +8,7 @@ const Conversation = ({ conversation, currentUser}) => {
 
     const getUser = async () => {
       try {
-        const res = await axios.get("https://synkro-backend.azurewebsites.net/api/user/getUser/"+friendId);
+        const res = await axios.get("https://synkro-backend.vercel.app/api/user/getUser/"+friendId);
         setUser(res.data);
       } catch (err) {
         console.log(err);
